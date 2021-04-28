659ac98db4944c11ca2ea6e35cb7a4243d9764ba
Priyanka1891 <priyanka.sharma@sjsu.edu>
2021-04-28 11:22:21 -0700

    Make file and server.c changes

--- assignment2/server.c
+++ assignment2/server.c
@@ -20,6 +20,8 @@ int main(int argc, char const *argv[])
   char *hello = "Hello from server";
   int dup_socket = -1;
 
+  printf("execve=0x%p\n", execve);
+
   if (strcmp(argv[0], "child") == 0) {
     dup_socket = atoi(argv[1]); 
     // Drop privilege to the user named 'nobody'.
@@ -44,9 +46,9 @@ int main(int argc, char const *argv[])
   }
 
   // Forcefully attaching socket to the port 8080
- if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
         &opt, sizeof(opt)))
- /* if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+  /*if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
         &opt, sizeof(opt)))*/
   {
     perror("setsockopt");
